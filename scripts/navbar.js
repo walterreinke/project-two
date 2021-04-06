5433ca59e63ded38abd8b2e540e1024adaa4016a
walterreinke <waltreinke@gmail.com>
2021-04-06 18:41:18 -0400

    finished creating the accordion for the coal page

---
+++ scripts/navbar.js
@@ -0,0 +1,19 @@
+// JS for the accordion taken from the w3schools link on the coal html file. the first part is creating the hover effect and changing the color of the button only when it is hovered. the second part is what is showing the panel when the button is clicked.
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
