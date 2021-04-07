7fd556d6835445cca0bd37ee513615896656dc47
walterreinke <waltreinke@gmail.com>
2021-04-06 22:36:50 -0400

    put content on the wind page

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -17,3 +17,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// this creates the animation for the accordion, code taken from same w3schools link. it is adding height to the panel when it is clicked on through the height that is created by the paragraph showing on screen.
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
