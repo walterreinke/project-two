07ed1fd1f031eac4f4c7697a512e26bb62c8e469
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-04-16 19:12:13 -0400

    Split JS into smaller files, import when needed.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,39 +1,3 @@
-// JS for the accordion taken from the w3schools link on the coal html file. the first part is creating the hover effect and changing the color of the button only when it is hovered. the second part is what is showing the panel when the button is clicked.
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-// this creates the animation for the accordion, code taken from same w3schools link. it is adding height to the panel when it is clicked on through the height that is created by the paragraph showing on screen.
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
 // js for the mobile part of the navbar, sourced from same website as in the index.html file. this makes the class change for the div that contains that navbar when clicked on, leading to the menu dropping because the added class has the css for the actual display.
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -42,37 +6,4 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
-}
-//   js for the grid slideshow, works pretty much the same as the slide show we made in class, including the functions that make sure you can't go past the last slide (taken from same w3schools website)
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active-two", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active-two";
 }
